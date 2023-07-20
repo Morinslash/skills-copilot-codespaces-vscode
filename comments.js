9277611b9adf8ca11c948f58d1faa1766358ea65
Sebastian Kulinski <41920857+Morinslash@users.noreply.github.com>
2023-07-20 14:59:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,90 @@
 // create web server
-var express = require('express');
-var app = express();
+// ==============================
 
-// create server
-var server = require('http').createServer(app);
+// import express
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const jwt = require('jsonwebtoken');
 
-// create socket
-var io = require('socket.io')(server);
-
-// listen to port 3000
-server.listen(3000);
-
-// create array of comments
-var comments = [
-    {name: 'John', comment: 'Hello'},
-    {name: 'Jack', comment: 'Hi'}
-];
-
-// use express to get static files
-app.use(express.static('./public'));
-
-// listen to connection
-io.on('connection', function(socket) {
-    // send comments to client
-    socket.emit('sendComments', comments);
-
-    // listen to addComment
-    socket.on('addComment', function(data) {
-        // add comment to array
-        comments.push(data);
-
-        // send updated comments to client
-        io.sockets.emit('sendComments', comments);
-    });
+// create a comment
+router.post('/', (req, res) => {
+    const token = req.headers['authorization'].split(' ')[1];
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    db.comment.create({
+        postId: req.body.postId,
+        userId: decoded.id,
+        body: req.body.body
+    })
+    .then((comment) => {
+        res.json({comment: comment});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    })
 });
+
+// get all comments
+router.get('/', (req, res) => {
+    db.comment.findAll()
+    .then((comments) => {
+        res.json(comments);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    })
+});
+
+// get all comments for a specific post
+router.get('/:postId', (req, res) => {
+    db.comment.findAll({
+        where: {
+            postId: req.params.postId
+        }
+    })
+    .then((comments) => {
+        res.json(comments);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    })
+});
+
+// update a comment
+router.put('/:id', (req, res) => {
+    db.comment.update({
+        body: req.body.body
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((updatedComment) => {
+        res.json(updatedComment);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    })
+});
+
+// delete a comment
+router.delete('/:id', (req, res) => {
+    db.comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => {
+        res.status(200).end();
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    })
+});
+
+module.exports = router;
