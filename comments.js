071d1aeeba87d1773aca9a8ecb2458158c72805f
Sebastian Kulinski <41920857+Morinslash@users.noreply.github.com>
2023-07-20 15:03:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,90 +1,20 @@
-// create web server
-// ==============================
+// create web server with one get route returning "hello world"
+// run server and access route in browser
 
-// import express
+// create a web server
 const express = require('express');
-const router = express.Router();
-const db = require('../models');
-const jwt = require('jsonwebtoken');
+const app = express();
 
-// create a comment
-router.post('/', (req, res) => {
-    const token = req.headers['authorization'].split(' ')[1];
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    db.comment.create({
-        postId: req.body.postId,
-        userId: decoded.id,
-        body: req.body.body
-    })
-    .then((comment) => {
-        res.json({comment: comment});
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).end();
-    })
+// create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-// get all comments
-router.get('/', (req, res) => {
-    db.comment.findAll()
-    .then((comments) => {
-        res.json(comments);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).end();
-    })
+// listen on port 3000
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
 
-// get all comments for a specific post
-router.get('/:postId', (req, res) => {
-    db.comment.findAll({
-        where: {
-            postId: req.params.postId
-        }
-    })
-    .then((comments) => {
-        res.json(comments);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).end();
-    })
-});
-
-// update a comment
-router.put('/:id', (req, res) => {
-    db.comment.update({
-        body: req.body.body
-    }, {
-        where: {
-            id: req.params.id
-        }
-    })
-    .then((updatedComment) => {
-        res.json(updatedComment);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).end();
-    })
-});
-
-// delete a comment
-router.delete('/:id', (req, res) => {
-    db.comment.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(() => {
-        res.status(200).end();
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).end();
-    })
-});
-
-module.exports = router;
+// run the server
+// node comments.js
+// open browser and go to http://localhost:3000/
